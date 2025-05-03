5d515490c2c1682b8a18e0fd8bb302e0beb27564
ddaokki <jeongmin0518@naver.com>
2025-05-04 08:46:46 +0900

    backend init
    build init envrionment

---
+++ backend/src/main/java/com/gamelier/backend/GamelierBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gamelier.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelierBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelierBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gamelier/backend/GamelierBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelier.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelierBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
